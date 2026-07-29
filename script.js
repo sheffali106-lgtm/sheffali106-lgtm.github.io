a8128bbe31eda5a3e3b8fd189291d067e82c569c
sheffali106-lgtm <sheffali106@gmail.com>
2026-07-29 23:50:44 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,8 @@
 document.querySelector("button").addEventListener("click", function () {
     window.open("https://wa.me/254700000000", "_blank");
 });
+document.querySelectorAll(".details-btn").forEach(button=>{
+button.onclick=()=>{
+alert("Property details page coming soon!");
+};
+});

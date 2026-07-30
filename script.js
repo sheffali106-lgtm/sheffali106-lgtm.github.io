141db542a77eb79ec44062f57a4092c04340e864
sheffali106-lgtm <sheffali106@gmail.com>
2026-07-30 11:33:47 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-document.querySelector("button").addEventListener("click", function () {
-    window.open("https://wa.me/254700000000", "_blank");
-});
+
 document.querySelectorAll(".details-btn").forEach(button=>{
 button.onclick=()=>{
 window.location.href="property.html";

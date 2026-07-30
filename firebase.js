135d1f6f63935b74721f22cd4ddd159b6e29321d
sheffali106-lgtm <sheffali106@gmail.com>
2026-07-30 10:39:23 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,1 +1,18 @@
 
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyAyd-NNlsWWf4GkBdhthXBLp2LWvRWvOiE",
+  authDomain: "renteasy-kenya.firebaseapp.com",
+  projectId: "renteasy-kenya",
+  storageBucket: "renteasy-kenya.firebasestorage.app",
+  messagingSenderId: "691155079697",
+  appId: "1:691155079697:web:8aa17df226b45ab776d7b0"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);

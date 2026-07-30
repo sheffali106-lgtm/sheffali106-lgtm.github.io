fadf6af3381632e26be34db3e64e5a5cd1c365b6
sheffali106-lgtm <sheffali106@gmail.com>
2026-07-30 10:12:01 +0300

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -5,3 +5,8 @@ const firebaseConfig = {
 };
 
 const app = initializeApp(firebaseConfig);
+import { getFirestore } from "https://www.gstatic.com/firebasejs/12.0.0/firebase-firestore.js";
+
+const db = getFirestore(app);
+
+export { db };

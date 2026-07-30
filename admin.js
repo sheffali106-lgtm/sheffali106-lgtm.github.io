8b2d7a29db9b29ebb33bb2e067178d01b344d53c
sheffali106-lgtm <sheffali106@gmail.com>
2026-07-30 10:59:55 +0300

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,3 +1,5 @@
+import { db } from "./firebase.js";
+import { collection, addDoc } from "https://www.gstatic.com/firebasejs/12.16.0/firebase-firestore.js";
 document.getElementById("propertyForm").addEventListener("submit", function(e){
 
 e.preventDefault();
